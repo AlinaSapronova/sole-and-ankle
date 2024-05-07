19ddc65168feb831221ee23d056b22fd7969cc67
alina <sapron4ik93@gmail.com>
2024-05-07 14:51:47 -0500

    done

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -8,12 +8,24 @@ const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
+      <ShoeWrapper>
         <ShoeCard key={shoe.slug} {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+display:flex;
+flex-wrap: wrap;
+margin: -16px;
+`;
+
+const ShoeWrapper = styled.div`
+min-width: 275px;
+flex: 1;
+margin: 16px;
+`;
 
 export default ShoeGrid;
